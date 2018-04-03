9e7a3102ff022cb72de9d0dfd9604482ea4f49a5
asuforce <owata.sn@gmail.com>
2018-04-03 22:40:01 +0900

    1.2.3
    add chat

---
+++ chat/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+	"path/filepath"
+	"sync"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(
+			template.ParseFiles(
+				filepath.Join("templates", t.filename),
+			),
+		)
+		if err := t.templ.Execute(w, nil); err != nil {
+			log.Fatal("TemplateExecution:", err)
+		}
+	})
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+
+//	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+//		w.Write([]byte(`
+//			<html>
+//				<head>
+//					<titleaチャット</title>
+//				</head>
+//				<body>
+//					チャットをしましょう！
+//				</body>
+//			</html>
+//		`))
+//	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+
+}
