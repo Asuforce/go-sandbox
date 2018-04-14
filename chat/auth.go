427f7ce54c68c8426aa518cb8827b17105a1d8c2
asuforce <owata.sn@gmail.com>
2018-04-14 15:41:02 +0900

    2.3
    create loginHandler

--- chat/auth.go
+++ chat/auth.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
 
 type authHandler struct {
 	next http.Handler
@@ -20,4 +25,27 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func MustAuth(handler http.Handler) http.Handler {
 	return &authHandler{next: handler}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	if action == "" {
+		w.WriteHeader(http.StatusNotFound)
+		log.Fatal("action is empty")
+	}
+
+	provider := segs[3]
+	if provider == "" {
+		w.WriteHeader(http.StatusNotFound)
+		log.Fatal("provider is empty")
+	}
+
+	switch action {
+	case "login":
+		log.Println("TODO: Login Process", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "%s is unsupported", action)
+	}
 }
