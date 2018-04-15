6cad535627621c8ef0913631bbcd6d89db893240
asuforce <owata.sn@gmail.com>
2018-04-15 11:27:57 +0900

    3.2.1.1
    create avatar test
    implemented GetAvatarURL

--- chat/avatar.go
+++ chat/avatar.go
@@ -7,3 +7,16 @@ var ErrNoAvatarURL = errors.New("chat: Failed to get Avatar URL")
 type Avatar interface {
 	GetAvatarURL(c *client) (string, error)
 }
+
+type AuthAvatar struct{}
+
+var UserAuthAvatar AuthAvatar
+
+func (AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
---
+++ chat/avatar_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestAuthAvatar(t *testing.T) {
+	var authAvatar AuthAvatar
+	client := new(client)
+	if _, err := authAvatar.GetAvatarURL(client); err != ErrNoAvatarURL {
+		t.Error("AuthAvatar.GetAvatarURL should return ErrNoAvatarURL when the value dosen't exist")
+	}
+
+	const testURL = "http://url-to-avatar/"
+	client.userData = map[string]interface{}{"avatar_url": testURL}
+	if url, err := authAvatar.GetAvatarURL(client); err != nil {
+		t.Error("AuthAvatar.GetAvatarURL should not return Error")
+	} else {
+		if url != testURL {
+			t.Error("AuthAvatar.GetAvatarURL should return correct URL")
+		}
+	}
+}
