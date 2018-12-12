fb0f6d58bd0067ded9383eddff5eb784c83c50fa
asuforce <owata.sn@gmail.com>
2018-12-12 18:30:58 +0900

    Divide directory

--- gin-gorm/main.go
+++ gin-gorm/main.go
@@ -1,91 +1,18 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/Asuforce/go-sandbox/gin-gorm/controllers"
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-// Person struct
-type Person struct {
-	ID        uint   `json:"id"`
-	FirstName string `json:"firstname"`
-	LastName  string `json:"lastname"`
-	City      string `json:"city"`
-}
-
-var (
-	db  *gorm.DB
-	err error
-)
-
 func main() {
-	db, err = gorm.Open("postgres", "host=0.0.0.0 port=5432 user=gorm dbname=gorm password=gorm sslmode=disable")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer db.Close()
-
-	db.AutoMigrate(&Person{})
-
 	r := gin.Default()
-	r.GET("/people", getPeople)
-	r.GET("/people/:id", getPerson)
-	r.POST("/people", createPerson)
-	r.PUT("/people/:id", updatePerson)
-	r.DELETE("/people/:id", deletePerson)
+	var ctrl person.Controller
+	r.GET("/people", ctrl.Index)
+	r.GET("/people/:id", ctrl.Show)
+	r.POST("/people", ctrl.Create)
+	r.PUT("/people/:id", ctrl.Update)
+	r.DELETE("/people/:id", ctrl.Delete)
 	r.Run()
-}
-
-func updatePerson(c *gin.Context) {
-	var person Person
-	id := c.Params.ByName("id")
-
-	if err := db.Where("id = ?", id).First(&person).Error; err != nil {
-		c.AbortWithStatus(404)
-		fmt.Println(err)
-	}
-	c.BindJSON(&person)
-
-	db.Save(&person)
-	c.JSON(200, person)
-}
-
-func deletePerson(c *gin.Context) {
-	id := c.Params.ByName("id")
-	var person Person
-	d := db.Where("id = ?", id).Delete(&person)
-	fmt.Println(d)
-	c.JSON(200, gin.H{"id #" + id: "deleted"})
-}
-
-func createPerson(c *gin.Context) {
-	var person Person
-	c.BindJSON(&person)
-
-	db.Create(&person)
-	c.JSON(200, person)
-}
-
-func getPerson(c *gin.Context) {
-	id := c.Params.ByName("id")
-	var person Person
-	if err := db.Where("id = ?", id).First(&person).Error; err != nil {
-		c.AbortWithStatus(404)
-		fmt.Println(err)
-	} else {
-		c.JSON(200, person)
-	}
-}
-
-func getPeople(c *gin.Context) {
-	var people []Person
-	if err := db.Find(&people).Error; err != nil {
-		c.AbortWithStatus(404)
-		fmt.Println(err)
-	} else {
-		c.JSON(200, people)
-	}
 }
