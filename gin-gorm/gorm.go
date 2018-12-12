f9fdd786ac79acbaaf9787ee27c8c58883559e20
asuforce <owata.sn@gmail.com>
2018-12-12 15:09:37 +0900

    Add migration

--- gin-gorm/gorm.go
+++ gin-gorm/gorm.go
@@ -19,9 +19,16 @@ func Gorm() {
 	db, _ := gorm.Open("sqlite3", "./gorm.db")
 	defer db.Close()
 
+	db.AutoMigrate(&Person{})
+
 	p1 := Person{FirstName: "John", LastName: "Doe"}
 	p2 := Person{FirstName: "Jane", LastName: "Smith"}
 
+	db.Create(&p1)
+	var p3 Person
+	db.First(&p3)
+
 	fmt.Println(p1.FirstName)
 	fmt.Println(p2.FirstName)
+	fmt.Println(p3.LastName)
 }
