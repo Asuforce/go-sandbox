7c187d8f4e11388d291141f4cb067747162a1763
asuforce <owata.sn@gmail.com>
2018-12-12 15:55:12 +0900

    Read all

--- gin-gorm/main.go
+++ gin-gorm/main.go
@@ -1,15 +1,45 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+// Person struct
+type Person struct {
+	ID        uint   `json:"id"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+var (
+	db  *gorm.DB
+	err error
+)
 
 func main() {
-	Gorm()
+	db, err := gorm.Open("sqlite3", "./gorm.db")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Person{})
 
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello, world",
-		})
-	})
+	r.GET("/people", getPeople)
 	r.Run()
+}
+
+func getPeople(c *gin.Context) {
+	var people []Person
+	if err := db.Find(&people).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, people)
+	}
 }
