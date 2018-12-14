58b253cb0ec37d935d0331c6f7de48786d372a13
asuforce <owata.sn@gmail.com>
2018-12-14 18:05:24 +0900

    Setup db

--- go-gin-boilerplate/db/db.go
+++ go-gin-boilerplate/db/db.go
@@ -1,23 +1,30 @@
 package db
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/vsouza/go-gin-boilerplate/config"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 var db *dynamodb.DynamoDB
 
 func Init() {
 	c := config.GetConfig()
-	db = dynamodb.New(session.New(&aws.Config{
-		Region:      aws.String(c.GetString("db.region")),
-		Credentials: credentials.NewEnvCredentials(),
-		Endpoint:    aws.String(c.GetString("db.endpoint")),
-		DisableSSL:  aws.Bool(c.GetBool("db.disable_ssl")),
-	}))
+
+	DBMS := c.GetString("DB.dbms")
+	HOST := c.GetString("DB.host")
+	USER := c.GetString("DB.user")
+	PASS := c.GetString("DB.pass")
+	DBNAME := c.GetString("DB.dbname")
+	SSLMODE := c.GetString("DB.sslmode")
+
+	CONNECT := "host=" + HOST + " port=" + PORT + " user=" + USER + " dbname=" + DBNAME + " password=" + PASS + " sslmode=" + SSLMODE
+
+	db, err = gorm.Open(DBMS, CONNECT)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func GetDB() *dynamodb.DynamoDB {
