856fd84e2e36216b907250e5ed555cc835127cb4
asuforce <owata.sn@gmail.com>
2020-03-27 18:19:19 +0900

    Create random pod name

--- job_creater/main.go
+++ job_creater/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
 	batchv1 "k8s.io/api/batch/v1"
 	v1 "k8s.io/api/core/v1"
@@ -9,6 +11,15 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+)
+
+var randSrc = rand.NewSource(time.Now().UnixNano())
+
+const (
+	rs   = "abcdefghijklmnopqrstuvwxyz"
+	bits = 6
+	mask = 1<<bits - 1
+	max  = 63 / bits
 )
 
 func main() {
@@ -26,16 +37,18 @@ func main() {
 
 	c := clientset.BatchV1().Jobs(namespace)
 
+	jobName := "pl-" + randString(10)
+
 	job := &batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "pi",
+			Name: jobName,
 		},
 		Spec: batchv1.JobSpec{
 			Template: v1.PodTemplateSpec{
 				Spec: v1.PodSpec{
 					Containers: []v1.Container{
 						{
-							Name:  "pi",
+							Name:  jobName,
 							Image: "perl",
 							Command: []string{
 								"perl",
@@ -66,3 +79,22 @@ func main() {
 }
 
 func int32Ptr(i int32) *int32 { return &i }
+
+func randString(n int) string {
+	b := make([]byte, n)
+	c, r := randSrc.Int63(), max
+	for i := n - 1; i >= 0; {
+		if r == 0 {
+			c, r = randSrc.Int63(), max
+		}
+		idx := int(c & mask)
+		if idx < len(rs) {
+			b[i] = rs[idx]
+			i--
+		}
+		c >>= bits
+		r--
+	}
+
+	return string(b)
+}
